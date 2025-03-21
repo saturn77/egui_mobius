1e719d584bea21d3a6acd7f4a3f38e08bd35dfb1
James Bonanno <james@atlantixeng.com>
2025-03-21 09:45:14 -0400

    fix: Add explicit lifetime parameters to Value::lock() to address deprecation warning

--- crates/egui_mobius/src/types.rs
+++ crates/egui_mobius/src/types.rs
@@ -109,7 +109,7 @@ impl<T> Clone for Value<T> {
 
 impl<T> Value<T> {
     // TODO avoid exposing `PoisonError` in the API here.
-    pub fn lock(&self) -> Result<ValueGuard<T>, PoisonError<MutexGuard<T>>> {
+    pub fn lock(&self) -> Result<ValueGuard<'_, T>, PoisonError<MutexGuard<'_, T>>> {
         let result = self.0.lock().map(|result| ValueGuard(result));
         result
     }
